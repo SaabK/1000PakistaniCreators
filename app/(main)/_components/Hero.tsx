67a73177cc60b7a47b565d1f0144f346d38050b8
Ali Bin Naseer <aliisinsane0089@gmail.com>
2024-06-30 16:46:01 +0300

    Fixed some stuff

--- app/(main)/_components/Hero.tsx
+++ app/(main)/_components/Hero.tsx
@@ -4,7 +4,7 @@ import React from "react";
 export default function Hero() {
     return (
         <section className="container md:w-[70%]">
-            <h1 className="text-xl md:text-4xl font-semibold text-center capitalize max-w-[700px] mx-auto">
+            <h1 className="text-lg md:text-4xl font-semibold text-center capitalize max-w-[700px] mx-auto">
                 join{" "}
                 <span className="text-myYellow">
                     10,000+ pakistani creators
@@ -14,7 +14,18 @@ export default function Hero() {
             </h1>
 
             <div className="flex flex-col items-center gap-5 mt-8 mx-auto">
-                <Newsletter />
+                <iframe
+                    src="https://embeds.beehiiv.com/b92a5d0f-829f-4083-8a96-1d8835cfdc06?slim=true"
+                    data-test-id="beehiiv-embed"
+                    height="52"
+                    scrolling="no"
+                    style={{
+                        margin: 0,
+                        borderRadius: "0px",
+                        backgroundColor: "transparent",
+                    }}
+                    className="w-4/5 md:w-3/5 min-w-80"
+                ></iframe>
 
                 <span className="text-gray-300 italic text-xs text-center w-[80%] md:w-full">
                     (the email might be in spam, promotion, social or update tab
--- app/_components/Footer/index.tsx
+++ app/_components/Footer/index.tsx
@@ -1,5 +1,16 @@
+import Link from "next/link";
 import React from "react";
 
 export default function Footer() {
-    return <div>Footer</div>;
+    return (
+        <footer className="text-xs text-center py-2 lowercase">
+            Made by{" "}
+            <Link
+                href="https://www.instagram.com/bakwasboye/"
+                className="text-myYellow border-b hover:border-b-2"
+            >
+                Ali Bin Naseer
+            </Link>
+        </footer>
+    );
 }
